4bed3f976fb9573426c8c3114e7df2a07c7fd037
IT22051448 <IT22051448@my.sliit.lk>
2025-03-07 20:04:11 +0530

    product test

--- Lightweight Smart Order Management App/Product Consumer/src/product/consumer/service/product_consumer/Activator.java
+++ Lightweight Smart Order Management App/Product Consumer/src/product/consumer/service/product_consumer/Activator.java
@@ -2,21 +2,47 @@ package product.consumer.service.product_consumer;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+
+import product.producer.service.ProductServiceImpl;
+import product.consumer.service.ProductConsumer;
 
 public class Activator implements BundleActivator {
 
-	private static BundleContext context;
+    private static BundleContext context;
+    private ProductConsumer productConsumer;  // Instance of ProductConsumer to invoke its logic
 
-	static BundleContext getContext() {
-		return context;
-	}
+    static BundleContext getContext() {
+        return context;
+    }
 
-	public void start(BundleContext bundleContext) throws Exception {
-		Activator.context = bundleContext;
-	}
+    @Override
+    public void start(BundleContext bundleContext) throws Exception {
+        Activator.context = bundleContext;
 
-	public void stop(BundleContext bundleContext) throws Exception {
-		Activator.context = null;
-	}
+        // Create an instance of ProductConsumer
+        productConsumer = new ProductConsumer();
 
+        // Reference the ProductServiceImpl using the OSGi service reference
+        ServiceReference<ProductServiceImpl> serviceReference = context.getServiceReference(ProductServiceImpl.class);
+        if (serviceReference != null) {
+            ProductServiceImpl productService = context.getService(serviceReference);
+            
+            // Inject the service and run the consumer logic
+            if (productService != null) {
+                productConsumer.start(bundleContext);  // Call the start method of ProductConsumer to simulate consuming the service
+            }
+        } else {
+            System.out.println("No Product Service found!");
+        }
+    }
+
+    @Override
+    public void stop(BundleContext bundleContext) throws Exception {
+        // Stop the ProductConsumer logic (clean up if necessary)
+        if (productConsumer != null) {
+            productConsumer.stop(bundleContext);
+        }
+        Activator.context = null;
+    }
 }
--- Lightweight Smart Order Management App/Product Producer/src/product/producer/service/product_producer/Activator.java
+++ Lightweight Smart Order Management App/Product Producer/src/product/producer/service/product_producer/Activator.java
@@ -2,21 +2,22 @@ package product.producer.service.product_producer;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+import product.producer.service.ProductServiceImpl;
 
 public class Activator implements BundleActivator {
+    private ServiceRegistration<ProductServiceImpl> serviceRegistration;
 
-	private static BundleContext context;
+    @Override
+    public void start(BundleContext context) throws Exception {
+        ProductServiceImpl productService = new ProductServiceImpl();
+        serviceRegistration = context.registerService(ProductServiceImpl.class, productService, null);
+        System.out.println("Product Producer started and service registered.");
+    }
 
-	static BundleContext getContext() {
-		return context;
-	}
-
-	public void start(BundleContext bundleContext) throws Exception {
-		Activator.context = bundleContext;
-	}
-
-	public void stop(BundleContext bundleContext) throws Exception {
-		Activator.context = null;
-	}
-
+    @Override
+    public void stop(BundleContext context) throws Exception {
+        serviceRegistration.unregister();
+        System.out.println("Product Producer stopped.");
+    }
 }
