0f542f8c67feced97aaa3d1eae475ce4e859b57a
IT22051448 <IT22051448@my.sliit.lk>
2025-03-08 23:21:26 +0530

    product producer created

--- Lightweight Smart Order Management App/Product Producer/src/product/producer/service/productproducer/Activator.java
+++ Lightweight Smart Order Management App/Product Producer/src/product/producer/service/productproducer/Activator.java
@@ -1,27 +1,36 @@
 package product.producer.service.productproducer;
 
-import java.io.Console;
-
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
 import org.osgi.framework.ServiceRegistration;
+import database.connector.service.databaseconnector.DatabaseService;
 
 
 public class Activator implements BundleActivator {
+	
     private ServiceRegistration<ProductServiceImpl> serviceRegistration;
-
+    private DatabaseService databaseService;
+    
     @Override
     public void start(BundleContext context) throws Exception {
-        ProductServiceImpl productService = new ProductServiceImpl();
-        serviceRegistration = context.registerService(ProductServiceImpl.class, productService, null);
-        System.out.println("Product Producer started and service registered.");
+    	ServiceReference<DatabaseService> ref = context.getServiceReference(DatabaseService.class);
+        if (ref != null) {
+            databaseService = context.getService(ref);
+            ProductServiceImpl productService = new ProductServiceImpl(databaseService);
+            serviceRegistration = context.registerService(ProductServiceImpl.class, productService, null);
+            System.out.println("Product Producer has started and service is registered.");
+        } else {
+            System.out.println("DatabaseService was not found. ProductService was not registered.");
+        }
     }
 
     @Override
     public void stop(BundleContext context) throws Exception {
-        serviceRegistration.unregister();
-        System.out.println("Product Producer stopped.");
+    	if (serviceRegistration != null) {
+            serviceRegistration.unregister();
+        }
+        System.out.println("Product Producer has stopped.");
     }
-    
     
 }
--- Lightweight Smart Order Management App/Product Producer/src/product/producer/service/productproducer/ProductServiceImpl.java
+++ Lightweight Smart Order Management App/Product Producer/src/product/producer/service/productproducer/ProductServiceImpl.java
@@ -2,17 +2,89 @@ package product.producer.service.productproducer;
 
 import java.util.ArrayList;
 import java.util.List;
+import database.connector.service.databaseconnector.DatabaseService;
+import java.sql.*;
+
 
 public class ProductServiceImpl {
-    private List<String> productList = new ArrayList<>();
-
-    public void addProduct(String name, double price) {
-        String product = name + " - $" + price;
-        productList.add(product);
-        System.out.println("Added Product: " + product);
-    }
-
-    public List<String> getProducts() {
+    
+	//database connection
+	private final DatabaseService databaseService;
+	
+	public ProductServiceImpl(DatabaseService databaseService) {
+		this.databaseService = databaseService;
+	}
+	
+	//Add a product
+	
+	public void addProduct(Product product) {
+		String sql = "INSERT INTO products VALUES (?, ?, ?, ?)";
+		try (Connection conn = databaseService.getConnection();
+				PreparedStatement prepstmt = conn.prepareStatement(sql) ) {
+			prepstmt.setString(1, product.getName());
+			prepstmt.setString(2,  product.getCategory());
+			prepstmt.setString(3, product.getDescription());
+			prepstmt.setDouble(4, product.getPrice());
+			prepstmt.executeUpdate();
+			System.out.println("Product added: " + product.getName());
+		} catch (SQLException error) {
+			error.printStackTrace();
+		}		
+	}
+	
+	//Get All Products
+	public List<Product> getProducts() {
+        List<Product> productList = new ArrayList<>();
+        String sql = "SELECT * FROM products";
+        try (Connection conn = databaseService.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql);
+             ResultSet rs = pstmt.executeQuery()) {
+            while (rs.next()) {
+                Product product = new Product(
+                        rs.getInt("id"),
+                        rs.getString("name"),
+                        rs.getString("category"),
+                        rs.getString("description"),
+                        rs.getDouble("price")
+                );
+                productList.add(product);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return productList;
     }
+	
+	//Update an Existing Product
+	public void updateProduct(Product product) {
+		String sql = "UPDATE products SET name = ?, category = ?, description = ?, price = ? WHERE ID = ?";
+		try (Connection conn = databaseService.getConnection();
+				PreparedStatement prepstmt = conn.prepareStatement(sql)) {
+			
+			prepstmt.setString(1, product.getName());
+			prepstmt.setString(2, product.getCategory());
+			prepstmt.setString(3, product.getDescription());
+			prepstmt.setDouble(4, product.getPrice());
+			prepstmt.setInt(4, product.getId());
+			prepstmt.executeUpdate();
+			System.out.println("Product updated: ID" + product.getId());
+		} catch (SQLException error) {
+			error.printStackTrace();
+		}
+	}
+	
+	//Delete an Existing Product
+	public void deleteProduct(int id) {
+        String sql = "DELETE FROM products WHERE id = ?";
+        try (Connection conn = databaseService.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setInt(1, id);
+            pstmt.executeUpdate();
+            System.out.println("Product deleted: ID " + id);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+	
+
 }
